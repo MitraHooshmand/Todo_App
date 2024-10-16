2582d50baf9819c316fd18b21d0ae1b484bc5b60
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-10-16 10:29:39 +0330

    data saved in local storage

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const todoTask = document.getElementById("task-input");
 const todoDate = document.getElementById("date-input");
 const addButton = document.querySelector(".add-button");
 const messagePlaceholdet = document.getElementById("message-placeholder");
-let dataArray = [];
-
+let dataArray = JSON.parse(localStorage.getItem("dataArray")) || [];
+console.log(dataArray);
+///////////////////
 const generateID = function () {
-  return Math.round(Math.random() * Math.random()*Math.pow(10, 15)).toString();
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(10, 15)
+  ).toString();
 };
+////////////////////////
 const showMessage = function (message, type) {
   messagePlaceholdet.innerHTML = "";
   const pTag = document.createElement("p");
@@ -19,6 +23,13 @@ const showMessage = function (message, type) {
     pTag.style.display = "none";
   }, 3000);
 };
+/////////////////////////
+
+const saveToLocalStorage = function () {
+  localStorage.setItem("dataArray", JSON.stringify(dataArray));
+};
+
+////////////////////////
 addButton.addEventListener("click", function () {
   const task = todoTask.value;
   const date = todoDate.value;
@@ -30,10 +41,11 @@ addButton.addEventListener("click", function () {
   };
   if (task) {
     dataArray.push(todoArray);
+    saveToLocalStorage();
     todoDate.value = todoTask.value = "";
     showMessage("Task added successfuly", "success");
   } else {
     showMessage("Please add a task!", "error");
   }
 });
-console.log(dataArray);
+//////////////////////////
